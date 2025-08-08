77d72dd296e8facb2563b38a9656fb640e5e79e5
DarioFGonzalez <dario.zerobyte@gmail.com>
2025-08-08 16:02:39 -0300

    wop: cambio en server.js para forzar el sync force

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,8 @@ const { MercadoPagoConfig } = require('mercadopago');
 const mainRouter = require('./routes/mainRouter');
 const client = new MercadoPagoConfig( { accessToken: process.env.MP_ACCESS_TOKEN } );
 
-server.use(express.json());
 server.use( cors() );
+server.use(express.json());
 server.use( (req,res,next) =>
 {
     req.mercadoPagoClient = client;
