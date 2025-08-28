bdd1d95bd71b235e97f0cb8ccd89398ba30c0fb8
DarioFGonzalez <dario.zerobyte@gmail.com>
2025-08-28 17:41:54 -0300

    feat: Botón 'agregar' modificado para que sea mas vistoso (cliente)

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -94,9 +94,16 @@ const Card: React.FC<CardProps> = ( { product } ) =>
         </button>
       </div>
 
-      <button className={Styles.buyButton} onClick={addToCart}>
-        Comprar
+      <button type="button" className={Styles.buyButton} onClick={addToCart}>
+        <span className={Styles.button__text}>Agregar</span>
+        <span className={Styles.button__icon}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className={Styles.svg}>
+            <line y2="19" y1="5" x2="12" x1="12"></line>
+            <line y2="12" y1="12" x2="19" x1="5"></line>
+          </svg>
+        </span>
       </button>
+
     </div>
   );
 };
