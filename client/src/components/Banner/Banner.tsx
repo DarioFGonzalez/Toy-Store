a193f24c913e0d9580285e88fa20e148d079bab7
DarioFGonzalez <dario.zerobyte@gmail.com>
2025-09-17 10:57:31 -0300

    fix: función toBanner agregada para el banner home

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -1,9 +1,9 @@
-import { Carousel } from 'react-bootstrap';
-import Styles from './Banner.module.css';
-import { useEffect, useState } from 'react';
-import type { Banner } from '../../types';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import type { Banner } from '../../types';
 import { URL } from '../../types/constants';
+import Styles from './Banner.module.css';
 
 const Banners: React.FC = () =>
 {
@@ -16,13 +16,19 @@ const Banners: React.FC = () =>
         .catch( ( err ) => console.log( err ) );
     }, [])
 
+    const toBanner = ( url: string): string =>
+    {
+      const transformedUrl = url.replace('/upload/', '/upload/w_960,h_400,c_fit,f_auto,q_auto/');
+      return transformedUrl;
+    }
+
     return(
     <div className={Styles.allCarrusel}>
         
         <Carousel interval={2000} pause="hover" slide={false}>
           {banners.map((item, index) => (
             <Carousel.Item key={index}>
-              <img src={item.imageUrl.url} alt={item.category} onClick={()=>console.log(item)}/>
+              <img src={toBanner(item.imageUrl.url)} alt={item.category} onClick={()=>console.log(item)}/>
             </Carousel.Item>
           ))}
         </Carousel>
